9b761cbdde7fd0678206d4e2de6faf076e02180c
Daniel Etter <etter.dt@gmail.com>
2018-05-11 16:19:17 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -21,7 +21,7 @@ def speak_to_grandma(phrase)
   elsif phrase == "What did you eat today?".upcase
     "NO, NOT SINCE 1938!"
   elsif phrase == "What?".upcase
-    "NO, NOT SINCE 1938"
+    "NO, NOT SINCE 1938!"
   else
     "HUH?! SPEAK UP, SONNY!"
 end
