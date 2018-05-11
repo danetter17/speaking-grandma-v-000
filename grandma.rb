49df8c2865db488048bba824d71faaa7275649e1
Daniel Etter <etter.dt@gmail.com>
2018-05-11 16:18:04 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -18,6 +18,8 @@ def speak_to_grandma(phrase)
     "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == "Hi Nana, how are you?"
     "HUH?! SPEAK UP, SONNY!"
+  elsif phrase == "WHAT DID YOU EAT TODAY?"
+    "NO, NOT SINCE 1938!"
   else
     "HUH?! SPEAK UP, SONNY!"
 end
